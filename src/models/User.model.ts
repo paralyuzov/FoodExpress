61f00c634d2413b6cd589d7a303cba2545b1c53b
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-07 23:25:48 +0300

    add User model interface

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -16,3 +16,17 @@ export interface UserResponse {
   user: User;
   access_token: string;
 }
+
+export interface RegisterDto {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  firstName: string;
+  lastName: string;
+  phone?: string;
+  role?: Role
+}
+
+export interface RegisterResponse {
+  message: string;
+}
