f8fde6e1e45f97d4484d4faa956c004933a10fec
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-07 23:27:04 +0300

    add email verification page

---
+++ src/app/user/verify-email-page/verify-email-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerifyEmailPage } from './verify-email-page';
+
+describe('VerifyEmailPage', () => {
+  let component: VerifyEmailPage;
+  let fixture: ComponentFixture<VerifyEmailPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VerifyEmailPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VerifyEmailPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/user/verify-email-page/verify-email-page.ts
@@ -0,0 +1,31 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectAuthError, selectAuthLoading, selectMessage } from '../../../store/auth/auth.selectors';
+import { AuthActions } from '../../../store/auth/auth.actions';
+@Component({
+  selector: 'app-verify-email-page',
+  imports: [RouterModule],
+  templateUrl: './verify-email-page.html',
+  styleUrl: './verify-email-page.css'
+})
+export class VerifyEmailPage implements OnInit  {
+  private readonly route = inject(ActivatedRoute);
+  private readonly store  = inject(Store);
+
+  readonly isLoading = this.store.selectSignal(selectAuthLoading);
+  readonly message = this.store.selectSignal(selectMessage);
+  readonly error = this.store.selectSignal(selectAuthError);
+
+  ngOnInit(): void {
+      const token = this.route.snapshot.queryParamMap.get('token');
+      if(token) {
+        this.store.dispatch(AuthActions.verifyEmail({token}))
+      }
+  }
+
+
+}
+
+
