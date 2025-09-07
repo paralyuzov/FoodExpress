ffe5bda1577eb5111d9caebe153abdfcca598cda
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-07 23:28:11 +0300

    update home component: remove nav and footer (moved to app level)

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { Nav } from "../ui/nav/nav";
 import { HeroSection } from "../ui/hero-section/hero-section";
 import { MainSection } from "../ui/main-section/main-section";
-import { Footer } from "../ui/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [Nav, HeroSection, MainSection, Footer],
+  imports: [ HeroSection, MainSection],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
