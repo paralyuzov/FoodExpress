45fecd5de1e22072e75916cefd7497b3feaa11b8
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-07 23:25:37 +0300

    setup routing: auth pages with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,18 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 
-export const routes: Routes = [{ path: '', component: Home }];
+export const routes: Routes = [
+  { path: '', component: Home },
+  {
+    path: 'auth/signup',
+    loadComponent: () => import('./user/register-page/register-page').then((m) => m.RegisterPage),
+  },
+  {
+    path: 'auth/login',
+    loadComponent: () => import('./user/login-page/login-page').then((m) => m.LoginPage),
+  },
+  {
+    path: 'auth/verify-email',
+    loadComponent: () => import('./user/verify-email-page/verify-email-page').then((m) => m.VerifyEmailPage),
+  },
+];
