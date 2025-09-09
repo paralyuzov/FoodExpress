0acb021134f8534e4d1df890bee377d8681ec4b7
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-10 00:56:53 +0300

    add restaurant page component

---
+++ src/app/resutaurant/restaurant-page/restaurant-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RestaurantPage } from './restaurant-page';
+
+describe('RestaurantPage', () => {
+  let component: RestaurantPage;
+  let fixture: ComponentFixture<RestaurantPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RestaurantPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RestaurantPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
