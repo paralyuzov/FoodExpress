19e7b1cd692e952c832c43f6b8eb99f80f84473f
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-12 15:13:56 +0300

    feat: connect dish cards to cart store

--- src/app/ui/dish-card/dish-card.ts
+++ src/app/ui/dish-card/dish-card.ts
@@ -1,13 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { Dish } from '../../../models';
+import { Store } from '@ngrx/store';
+import { cartAction } from '../../../store/cart/cart.actions';
+
 
 @Component({
   selector: 'app-dish-card',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe,],
   templateUrl: './dish-card.html',
-  styleUrl: './dish-card.css'
+  styleUrl: './dish-card.css',
+  providers: [],
 })
 export class DishCard {
   dish = input.required<Dish>();
+  private store = inject(Store);
+
+  addToCart(dish: Dish) {
+    this.store.dispatch(cartAction.addItemToCart({dish}));
+  }
 }
