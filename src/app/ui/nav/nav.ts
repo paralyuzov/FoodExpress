b428ce9e7f356a5e935ebec5acc2abeb99cc4443
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-12 15:13:39 +0300

    feat: add custom cart button with badge

--- src/app/ui/nav/nav.ts
+++ src/app/ui/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { TieredMenuModule } from 'primeng/tieredmenu';
 import { Store } from '@ngrx/store';
@@ -6,10 +6,14 @@ import { selectIsAuthenticated, selectUser } from '../../../store/auth/auth.sele
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { AuthActions } from '../../../store/auth/auth.actions';
+import { AvatarModule } from 'primeng/avatar';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { CartDrawer } from "../cart-drawer/cart-drawer";
+import { selectCartItemCount } from '../../../store/cart/cart.selectors';
 
 @Component({
   selector: 'app-nav',
-  imports: [TieredMenuModule, RouterModule],
+  imports: [TieredMenuModule, RouterModule, AvatarModule, OverlayBadgeModule, CartDrawer],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
@@ -17,6 +21,8 @@ export class Nav implements OnInit {
   items: MenuItem[] | undefined;
   private store = inject(Store);
   private router = inject(Router);
+  openCartDrawer = signal(false);
+  cartItemsCount = this.store.selectSignal(selectCartItemCount);
 
   isAuthenticated = this.store.selectSignal(selectIsAuthenticated);
   user = this.store.selectSignal(selectUser)
@@ -42,6 +48,14 @@ export class Nav implements OnInit {
 
   navigateToAuth() {
     this.router.navigate(['auth/signup']);
+  }
+
+  openCart() {
+    this.openCartDrawer.set(true);
+  }
+
+  onCartVisibilityChange(visible: boolean) {
+    this.openCartDrawer.set(visible);
   }
 
 }
