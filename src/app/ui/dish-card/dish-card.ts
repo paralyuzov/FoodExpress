9de856a26ee0c9d7971e073451aa88fc7ba0ea9f
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-17 18:54:52 +0300

    feat: add rating component to dish cards
    - Add PrimeNG rating component with amber styling
    - Implement onRate method with dish rating dispatch
    - Add authentication check to show rating only for logged-in users
    - Update dish card template with rating count display
    - Import required rating modules and auth selectors

--- src/app/ui/dish-card/dish-card.ts
+++ src/app/ui/dish-card/dish-card.ts
@@ -1,22 +1,36 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, effect, inject, input, signal } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { Dish } from '../../../models';
 import { Store } from '@ngrx/store';
 import { cartAction } from '../../../store/cart/cart.actions';
+import { RatingModule, RatingRateEvent } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+import { selectIsAuthenticated } from '../../../store/auth/auth.selectors';
+import { dishActions } from '../../../store/dish/dish.actions';
 
 
 @Component({
   selector: 'app-dish-card',
-  imports: [DecimalPipe,],
+  imports: [DecimalPipe, RatingModule, FormsModule],
   templateUrl: './dish-card.html',
   styleUrl: './dish-card.css',
-  providers: [],
 })
 export class DishCard {
   dish = input.required<Dish>();
   private store = inject(Store);
+  rateValue = signal<number>(0);
+  isAuth = this.store.selectSignal(selectIsAuthenticated);
 
   addToCart(dish: Dish) {
     this.store.dispatch(cartAction.addItemToCart({dish}));
+  }
+
+
+  onRate(event: RatingRateEvent, dishId: string) {
+    const value = event.value;
+    if (value > 0 && value <= 5) {
+      this.rateValue.set(value);
+      this.store.dispatch(dishActions.rateDish({ dishId, rating: value }));
+    }
   }
 }
