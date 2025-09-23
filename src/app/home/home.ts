de246ac98f58406c1ca586e855cca79a5e3c13b9
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-23 13:00:22 +0300

    feat(home): enhance home component layout and functionality

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { HeroSection } from "../ui/hero-section/hero-section";
 import { MainSection } from "../ui/main-section/main-section";
+import { Store } from '@ngrx/store';
+import { selectAllDishes, selectDishesLoading } from '../../store/dish/dish.selectors';
+import { dishActions } from '../../store/dish/dish.actions';
+import { selectAllRestaurants, selectRestaurantLoading } from '../../store/restaurant/restaurant.selectors';
+import { restaurantAction } from '../../store/restaurant/restaurant.actions';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +13,18 @@ import { MainSection } from "../ui/main-section/main-section";
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
+
+  private store = inject(Store);
+
+  dishes = this.store.selectSignal(selectAllDishes);
+  loadingDishes = this.store.selectSignal(selectDishesLoading);
+  restaurants = this.store.selectSignal(selectAllRestaurants);
+  loadingRestaurants = this.store.selectSignal(selectRestaurantLoading);
+
+  ngOnInit() {
+    this.store.dispatch(dishActions.loadMostPopularDishes({ limit: 10 }));
+    this.store.dispatch(restaurantAction.loadMostPopularRestaurants({ limit: 10 }));
+  }
 
 }
