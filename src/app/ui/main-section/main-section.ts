efe67908a8ab722e38923547a976fa7a9400df39
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-09-23 12:58:04 +0300

    feat: add restaurant carousel and popular dishes data

--- src/app/ui/main-section/main-section.ts
+++ src/app/ui/main-section/main-section.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input, signal, computed, effect } from '@angular/core';
+import { Dish, Restaurant } from '../../../models';
+import { RouterModule } from '@angular/router';
+import { Carousel } from 'primeng/carousel';
+import { Rating } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-main-section',
-  imports: [],
+  imports: [RouterModule, Carousel, Rating,FormsModule],
   templateUrl: './main-section.html',
-  styleUrl: './main-section.css'
+  styleUrl: './main-section.css',
 })
 export class MainSection {
-  
+  dishes = input.required<Dish[]>();
+  loadingDishes = input.required<boolean>();
+
+  restaurants = input.required<Restaurant[]>();
+  loadingRestaurants = input.required<boolean>();
 }
